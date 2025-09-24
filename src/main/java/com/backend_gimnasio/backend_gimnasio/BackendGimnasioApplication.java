6d8412a10983615a0902185adbd4c421fbe76e16
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2025-09-24 11:13:43 -0300

    first commit

---
+++ src/main/java/com/backend_gimnasio/backend_gimnasio/BackendGimnasioApplication.java
@@ -0,0 +1,13 @@
+package com.backend_gimnasio.backend_gimnasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGimnasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGimnasioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend_gimnasio/backend_gimnasio/BackendGimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_gimnasio.backend_gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
